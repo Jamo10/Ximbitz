99bafa06a07efbbc459fbf97991bfedd659f4426
Je'l Awesh Medrano Otzoy <ja.medrano@zohomail.com>
2021-10-26 23:08:31 -0600

    Nuevo diseño de página

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
-document.getElementById('productos').style.display = 'none';
-document.getElementById('contacto').style.display = 'none';
+/* SHOW MENU */
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId);
 
+  if(toggle && nav){
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show-menu');
+    })
+  }
+}
 
-Mostrar = (value) => {
-    document.getElementById('nosotros').style.display = 'none';
-    document.getElementById('productos').style.display = 'none';
-    document.getElementById('contacto').style.display = 'none';
-
-    document.getElementById(value).style.display = 'block';
-  } 
+showMenu('nav-toggle', 'nav-menu');
